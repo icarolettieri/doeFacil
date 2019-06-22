7f2dcea19874da8b816dc4e312d3b623c917294a
Ícaro Lettieri <icaro_lettieri@live.com>
2019-06-22 00:06:24 -0300

    tentativa de conexao com banco remoto

--- php/class/Banco.class.php
+++ php/class/Banco.class.php
@@ -1,7 +1,7 @@
 <?php
 	class Banco{
 		protected $pdo;
-
+		//Teste conexão remota hospedagem
 		public function __construct(){
 			$this->pdo = new PDO("mysql:dbname=u776920662_pidoe;host=172.25.6.235","u776920662_icaro","Pi01.2019");
 		}

9ea1a3ead3a4cae355fad8b51fc5e1eb224e6ec4
Ícaro Lettieri <icaro_lettieri@live.com>
2019-06-22 00:38:10 -0300

    upload teste conexao com banco

---
+++ mysqltest.php
@@ -0,0 +1,12 @@
+<?
+$banco = "u776920662_pidoe";
+$usuario = "u776920662_icaro";
+$senha = "DoeFacil123";
+$hostname = "sql210.main-hosting.eu";
+$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( "Não foi possível conectar ao banco MySQL");
+if (!$conn) {echo "Não foi possível conectar ao banco MySQL.
+"; exit;}
+else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
+";}
+mysql_close();
+?>

8b170df7dbb770dc5ee6a86a4f386d1de7a03a76
Ícaro Lettieri <icaro_lettieri@live.com>
2019-06-24 15:07:36 -0300

    teste conexão

--- mysqltest.php
+++ mysqltest.php
@@ -2,11 +2,11 @@
 $banco = "u776920662_pidoe";
 $usuario = "u776920662_icaro";
 $senha = "DoeFacil123";
-$hostname = "sql210.main-hosting.eu";
-$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( "N�o foi poss�vel conectar ao banco MySQL");
-if (!$conn) {echo "N�o foi poss�vel conectar ao banco MySQL.
+$hostname = "localhost";
+$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( "Nao foi possivel conectar ao banco MySQL");
+if (!$conn) {echo "Nao foi possivel conectar ao banco MySQL.
 "; exit;}
-else {echo "Parab�ns!! A conex�o ao banco de dados ocorreu normalmente!.
+else {echo "Parabens!! A conexao ao banco de dados ocorreu normalmente!.
 ";}
 mysql_close();
 ?>

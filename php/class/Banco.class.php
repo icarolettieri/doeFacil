ba01154ea84185ddf5bdc880f8ae50b5cef3216d
Ícaro Lettieri <icaro_lettieri@live.com>
2019-06-24 15:16:37 -0300

    alteração conexão com banco

--- php/class/Banco.class.php
+++ php/class/Banco.class.php
@@ -3,11 +3,11 @@
 		protected $pdo;
 		//Teste conexão remota hospedagem
 		public function __construct(){
-			$this->pdo = new PDO("mysql:
-				dbname=u776920662_pidoe;
-				host=localhost",
-				"u776920662_icaro",
-				"DoeFacil123");
+			$this->pdo = new PDO('
+				mysql:host=localhost;dbname=u776920662_pidoe',
+				'u776920662_icaro', 'DoeFacil123',
+				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			);
 		}
 
 	}

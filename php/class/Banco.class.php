a9b70588132e5eb00bdbee4bb14802f3c7e090ae
IcaroLettieri <contact@icarolettieri.com>
2019-06-24 13:15:54 -0300

    Update Banco.class.php

--- php/class/Banco.class.php
+++ php/class/Banco.class.php
@@ -4,10 +4,10 @@
 		//Teste conexão remota hospedagem
 		public function __construct(){
 			$this->pdo = new PDO("mysql:
-														dbname=u776920662_pidoe;
-														host=sql210.main-hosting.eu",
-														"u776920662_icaro",
-														"DoeFacil123");
+				dbname=u776920662_pidoe;
+				host=sql210.main-hosting.eu",
+				"u776920662_icaro",
+				"DoeFacil123");
 		}
 
 	}
